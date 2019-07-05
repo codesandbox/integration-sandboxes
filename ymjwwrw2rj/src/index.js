716fbddf30bf523ee3a4eb08502b436425d92ef2
Bogdan Luca <luca.bogdan@gmail.com>
2019-07-05 14:30:09 +0300

    Add "ymjwwrw2rj" (empty path) sandbox.

---
+++ ymjwwrw2rj/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./main";
+
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Main />, rootElement);
---
+++ ymjwwrw2rj/src/main.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import BulbOn from "";
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div>
+        <BulbOn />
+      </div>
+    );
+  }
+}
