dd82ad66933de90168c347debdb3d51b649d5d42
Bogdan Luca <luca.bogdan@gmail.com>
2019-07-05 13:57:03 +0300

    Add "k3q1zjjml5" (vue slideout) sandbox.

---
+++ k3q1zjjml5/components/Hello.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="hello">
+		<nav id="menu">
+			<div>Menu</div>
+		</nav>
+
+		<main id="panel">
+			<header>
+				<div><button class="toggle-button">☰</button>Panel</div>
+				<div class="another-toggle"> <button>another</button></div>
+				<div class="not-a-toggle"> <button>this one wont work</button></div>
+			</header>
+		</main>
+	</div>
+</template>
+
+<script>
+	export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
