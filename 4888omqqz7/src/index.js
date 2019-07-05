39f8db8575e299913573758ab0b5112ed8326048
Bogdan Luca <luca.bogdan@gmail.com>
2019-07-05 14:42:06 +0300

    Add "4888omqqz7" (material-ui) sandbox.

---
+++ 4888omqqz7/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Demo from "./demo";
+
+console.log("--", document.querySelector("#root"));
+
+ReactDOM.render(<Demo />, document.querySelector("#root"));
