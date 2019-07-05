40733386d912f48e2c49ba4470965717a5c7811b
Bogdan Luca <luca.bogdan@gmail.com>
2019-07-05 13:45:13 +0300

    Add "react-ts" sandbox.

---
+++ react-ts/src/index.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
