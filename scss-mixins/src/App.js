e7424096312411c9e49e2449610b95dd2ff3561b
Jasper De Moor <jasperdemoor@gmail.com>
2021-07-26 11:00:21 +0200

    add scss mixin test

---
+++ scss-mixins/src/App.js
@@ -0,0 +1,12 @@
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Edit to see some magic happen!</h2>
+
+      <button className="button">Click me!</button>
+    </div>
+  );
+}
