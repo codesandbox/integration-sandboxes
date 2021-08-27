852d45124c002251b1057d0e05ffdc3487a57c8c
Jasper De Moor <jasperdemoor@gmail.com>
2021-08-27 11:26:09 +0200

    add vue 2 modules test

---
+++ vue-2-css-module-vuzkt/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div :class="$style.red">
+      This text should be red if css modules works
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style module>
+.red {
+  color: red;
+}
+</style>
