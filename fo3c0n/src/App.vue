ba679dd32e2edb39f3b87a910d27999f4be293df
Danilo Woznica <danilowoz@gmail.com>
2022-11-29 14:17:13 +0000

    Create 5 files

---
+++ fo3c0n/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const msg = ref("world");
+</script>
+
+<template>
+  <h1>Hello {{ msg }}</h1>
+</template>
+
+<style>
+h1 {
+  font-size: 1.5rem;
+}
+</style>
